38550a0f28b85c7f1eff181086d54547c7ed79c7
Nick Cave <nxdmedia@googlemail.com>
2020-03-31 21:11:42 +0100

    Setting up fetch request

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 // ----------------------------------------------------
 // DECLARING VARIABLES
 // ----------------------------------------------------
-const url = 'https://randomuser.me/api/?results=12';
-
+const url = 'https://randomuser.me/api/?results=12&inc=name,picture,email,location,cell,dob&nat=gb';
+const container = document.getElementById('page-container');
 
 
 // ----------------------------------------------------
 // FETCH FUNCTION
 // ----------------------------------------------------
-
 fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => generateHTML(data))
 
 
 
@@ -19,4 +18,20 @@ fetch(url)
 // ----------------------------------------------------
 // FUNCTIONS
 // ----------------------------------------------------
-
+function generateHTML(data) {
+    let html;
+    for (let i = 0; i < data.results.length; i++) {
+        console.log(data.results[i]);
+        html = `
+            <div class="employee-container">
+                <img src="${data.results[i].picture.large}">
+                <div>
+                    <p>${data.results[i].name.first} ${data.results[i].name.last}</p>
+                    <p>${data.results[i].email}</p>
+                    <p>${data.results[i].location.city}</p>
+                <div>
+            </div>
+        `;
+        container.innerHTML += html;
+    }
+};
